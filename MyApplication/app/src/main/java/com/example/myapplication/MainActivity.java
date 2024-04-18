c4323e2578d7216feaa2a898316e661590ff872d
Heng <u7611510@anu.edu.au>
2024-04-18 23:03:01 +1000

    配置完了apikey和gradle.kts,导入了相关包在libs中

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String id = "u7611510";
+        Button startExerciseButton = findViewById(R.id.startExerciseButton);
+        startExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Start Exercise",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), ExerciseActivity.class);
+                intent.putExtra("id",id);
+                startActivity(intent);
+            }
+        });
+    }
+}
