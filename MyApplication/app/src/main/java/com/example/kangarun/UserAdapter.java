07eccdb268e54f228f72369398cfe5a8c843ab96
Runyao Wang <u6812566@anu.edu.au>
2024-05-01 01:35:56 +1000

    Fix friend list profile picture

--- MyApplication/app/src/main/java/com/example/kangarun/UserAdapter.java
+++ MyApplication/app/src/main/java/com/example/kangarun/UserAdapter.java
@@ -2,6 +2,7 @@ package com.example.kangarun;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -9,6 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kangarun.databinding.UserContainerBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.Base64;
 import java.util.List;
@@ -62,6 +67,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         void setUserData(User user) {
             binding.textName.setText(user.getUsername());
             binding.textEmail.setText(user.getEmail());
+            StorageReference fileRef = FirebaseStorage.getInstance().getReference()
+                            .child("user/" + user.getUserId() + "/profile.jpg");
+            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.get().load(uri).into(binding.imageProfile);
+                }
+            });
+
             //binding.imageProfile.setImageBitmap(getBitmapFromEncoded(user.getProfilePicture()));TODO
             binding.getRoot().setOnClickListener(v -> userListener.onUserClicked(user));
         }
