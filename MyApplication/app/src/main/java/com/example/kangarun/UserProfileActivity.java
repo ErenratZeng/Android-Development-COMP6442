a719a3ba587fd853fea756f93e2681c25ab58805
QIUTONG ZENG <zengqiutong@outlook.com>
2024-04-30 14:57:19 +1000

    Implement upload picture function, but it cannot be saved to cloud so far

--- MyApplication/app/src/main/java/com/example/kangarun/UserProfileActivity.java
+++ MyApplication/app/src/main/java/com/example/kangarun/UserProfileActivity.java
@@ -1,13 +1,23 @@
 package com.example.kangarun;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -18,9 +28,11 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class UserProfileActivity extends AppCompatActivity {
     TextView useremail,username,usergender,userweight,userheight;
-    private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
-
+    Button uploadImageButton;
+    ImageView profile_image_view;
+    ActivityResultLauncher<Intent> selectGallary;
+    Uri select;
     public UserProfileActivity(){
 
     }
@@ -31,8 +43,9 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
         username = findViewById(R.id.username);
         useremail = findViewById(R.id.useremail);
+        profile_image_view = findViewById(R.id.profile_image_view);
+        uploadImageButton = findViewById(R.id.uploadImageButton);
 
-        firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
         DocumentReference documentReference = firebaseFirestore.collection("user").document(User.getCurrentUserId());
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
@@ -43,5 +56,23 @@ public class UserProfileActivity extends AppCompatActivity {
                 //TODO Add more text
             }
         });
+
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(UserProfileActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        profile_image_view.setImageURI(uri);
     }
 }
