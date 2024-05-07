714d4e916f044f887b5be2cb2ffc06099a9b547d
Yan Jin <u7779907@anu.edu.au>
2024-05-07 16:09:44 +1000

    main interface revise

--- MyApplication/app/src/main/java/com/example/kangarun/activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/kangarun/activity/MainActivity.java
@@ -17,40 +17,39 @@ import com.example.kangarun.utils.UserAVLTree;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     public static UserAVLTree tree;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tree = new UserAVLTree();
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.inflateMenu(R.menu.bottom_nav_menu);
-        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                int id = item.getItemId();
-
-                if (id == R.id.navigation_search) {
-                    selectedFragment = new SearchFragment();
-                } else if (id == R.id.navigation_sports) {
-                    selectedFragment = new SportsFragment();
-                } else if (id == R.id.navigation_friends) {
-                    selectedFragment = new FriendsFragment();
-                } else if (id == R.id.navigation_profile) {
-                    selectedFragment = new ProfileFragment();
-                }
-                if (selectedFragment != null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-                }
-                return false;
+        navView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_search) {
+                selectedFragment = new SearchFragment();
+            } else if (itemId == R.id.navigation_sports) {
+                selectedFragment = new SportsFragment();
+            } else if (itemId == R.id.navigation_friends) {
+                selectedFragment = new FriendsFragment();
+            } else if (itemId == R.id.navigation_profile) {
+                selectedFragment = new ProfileFragment();
             }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+            return false;
         });
 
-        // Ensure the first fragment is loaded by default (if not redirected to Search)
+        // 默认加载 ProfileFragment
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
         }
     }
 }
+
